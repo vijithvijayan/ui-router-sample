19202ec077912a25ece23ba111684e551d65aae4
Vijith Vijayan <vijith.vijayan@cuelogic.co.in>
2016-04-15 11:49:10 +0530

    sample ui-router app

---
+++ public/app.js
@@ -0,0 +1,16 @@
+var routerApp = angular.module('routerApp', ['ui.router'])
+
+routerApp.config(function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider
+		.state('home', {
+			url:'/home',
+			templateUrl:'partial-home.html'
+		})
+		.state('about', {
+			// url:'';
+			// templateUrl:''
+		})
+
+})
